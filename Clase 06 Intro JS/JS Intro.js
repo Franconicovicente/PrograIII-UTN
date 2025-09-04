77696e6ca64c13bc0251640736652e4b1cae7760
Franconicovicente <franconicolassvicente@gmail.com>
2025-09-04 13:35:26 -0300

    Clase JS y Actualizacion CV online

---
+++ Clase 06 Intro JS/JS Intro.js
@@ -0,0 +1,54 @@
+// Es un lenguaje de programacion que ayuda a crear paginas web interactivas
+console.log("Hola mundo!");
+
+/*
+
+Tipos de variables
+
+- var:  Historica definicion de variables, tiene limitaciones
+
+- let: Permite declarar variables que pueden cambiar y tiene alcance de bloque
+
+- const: Asigna variables que no se deben reasignar 
+
+*/
+
+/*
+Tipos de datos
+
+- Numeros: Valores numericos
+
+- Cadenas: Texto cerrado entre comillas simples o dobles
+
+- Booleanos: true/false
+
+- null: Representa el valor intencionalmente vacio
+
+- undefined: Variable declarada pero que no tiene valor
+
+
+
+*/
+
+let numero = 42;
+let texto = "Hola";
+let verdadero = true;
+let vacio = null;
+let indefinido;
+
+console.log(numero);
+console.log(texto);
+console.log(verdadero);
+console.log(vacio);
+console.log(indefinido);
+
+/* 
+Operadores en JS
+
+- Aritmeticos
+- De asignacion
+- De comparacion
+- Logicos
+- De tipo (typeof, instanceof)
+- Incremento/Decremento
+*/
