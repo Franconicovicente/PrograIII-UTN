121d140df56b761c0ac21bcea15362ca24d20dc1
Franconicovicente <franconicolassvicente@gmail.com>
2025-09-11 23:02:52 -0300

    Ejercicios Parte 01

---
+++ Ejercicios Clase 07/Ejercicios.js
@@ -0,0 +1,88 @@
+// 1. Convertir a mayúsculas
+let texto = "hola mundo";
+// // Resultado: "HOLA MUNDO"
+
+console.log(texto.toUpperCase());
+
+// 2. Reemplazar una palabra
+let saludo = "Hola Juan";
+// // Resultado: "Hola Ana"
+
+console.log(saludo.replace("Juan", "Ana"));
+
+// 3. Obtener la longitud del string
+let palabra3 = "programación";
+// // Resultado: 12
+
+console.log(palabra3.length);
+
+// 4. Extraer una subcadena
+let frase = "JavaScript es divertido";
+// // Resultado: "JavaScript"
+
+console.log(frase.slice(0, 10));
+
+// 5. Verificar si incluye una palabra
+let mensaje = "Aprendiendo JavaScript";
+// // Resultado: true
+
+console.log(mensaje.includes("JavaScript"));
+
+// 6. Convertir a minúsculas
+let titulo = "HOLA MUNDO";
+// // Resultado: "hola mundo"
+
+console.log(titulo.toLowerCase());
+
+// 7. Eliminar espacios al inicio y final
+
+let entrada = " texto limpio ";
+
+// // Resultado: "texto limpio"
+
+console.log(entrada.trim());
+
+// 8. Repetir un string
+let risa = "ja";
+// // Resultado: "jajaja"
+
+console.log(risa.concat("jaja"));
+
+// 9. Dividir un string en array
+let nombres = "Ana,Juan,Pedro";
+// // Resultado: ["Ana", "Juan", "Pedro"]
+
+console.log(nombres.split(","));
+
+// 10. Obtener el carácter en una posición
+let palabra = "Hola";
+// // Resultado: "o"
+
+console.log(palabra.charAt(1));
+
+// Métodos de Arrays
+
+// 11. Agregar elemento al final
+// let frutas = ["manzana", "pera"];
+// // Resultado: ["manzana", "pera", "uva"]
+
+// 12. Eliminar el último elemento y devolverlo en una variable
+// let numeros = [1, 2, 3];
+// // Resultado: 3
+
+// 13. Agregar al principio
+// let letras = ["b", "c"];
+// // Resultado: ["a", "b", "c"]
+
+// 14. Eliminar el primer elemento y devolverlo en una variable
+// let datos = [10, 20, 30];
+
+// // Resultado: 10
+
+// 15. Verificar si incluye un valor
+// let lista = ["pan", "leche"];
+// // Resultado: leche devuelve true
+
+// 16. Unir elementos del array
+// let palabras = ["Hola", "mundo"];
+// // Resultado: "Hola mundo"
